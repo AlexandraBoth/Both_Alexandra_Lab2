371066cdccd7073143817807ca578c2f55ad6d58
Alexandra <alexandra.both@stud.ubbcluj.ro>
2022-11-14 13:47:28 +0200

    Final

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,11 +1,21 @@
-﻿namespace Both_Alexandra_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Both_Alexandra_Lab2.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string FullName { get; set; }
+        [Display(Name = "Full Name")] 
+        public string? FullName 
+        { 
+            get 
+            { 
+                return FirstName + " " + LastName; 
+            } 
+        }
         public ICollection<Book>? Books { get; set; }
     }
 }
