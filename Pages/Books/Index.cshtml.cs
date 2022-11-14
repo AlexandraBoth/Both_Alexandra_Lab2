5a40b1c6e2fb72e315fb8d8f5eb9a5ef851cc6e1
Alexandra <alexandra.both@stud.ubbcluj.ro>
2022-11-14 14:13:09 +0200

    cg

--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -28,6 +28,7 @@ namespace Both_Alexandra_Lab2.Pages.Books
             BookD = new BookData();
             BookD.Books = await _context.Book
                 .Include(b => b.Publisher)
+                .Include(b => b.Author)
                 .Include(b => b.BookCategories)
                 .ThenInclude(b => b.Category)
                 .AsNoTracking()
